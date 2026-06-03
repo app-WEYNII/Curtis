002f4c76cf0b663a6caafcf66cd19ea9b4f22451
app-WEYNII <lucadjouadii@gmail.com>
2026-06-03 09:47:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Stratégie : cache-first pour la coquille, network-first pour
    les ressources externes (fonts).
    ============================================================ */
-const CACHE_VERSION = 'ma-grossesse-v2';
+const CACHE_VERSION = 'ma-grossesse-v3';
 const CORE_ASSETS = [
   './',
   './index.html',
